117e1bbab9011693cb85a57fe6ed5721d2e94322
SilentKnight1994 <virtualtrader1994@gmail.com>
2026-03-06 19:56:43 +0530

    Add Arbitrum Sepolia network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    arbitrumSepolia: {
+      url: "https://sepolia-rollup.arbitrum.io/rpc",
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
+      chainId: 421614,
+    },
+  },
 };
